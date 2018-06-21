8fd2386a5e2d7f99563b119d76447bafa0ecda89
egalland <emmanuel.galland117@gmail.com>
2018-06-21 15:31:40 +0200

    Exercice 1

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+var image = document.getElementById('image1');
+
+function mouseOver() {
+  image.setAttribute('src', 'assets/images/image1_2.jpg');
+}
+
+function mouseOut() {
+  image.setAttribute('src', 'assets/images/image1.jpg');
+}
