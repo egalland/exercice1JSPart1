c3e2faf469c8afa17a4fe41ce6bf5c9128ce90ae
egalland <emmanuel.galland117@gmail.com>
2018-06-22 08:48:24 +0200

    maj commentaires

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,10 @@
+// DECLARATION ET RECUPERATION DES VARIABLES
 var image = document.getElementById('image1');
-
+// FONCTION LORSQU'ON MET LA SOURIS SUR L'IMAGE
 function mouseOver() {
   image.setAttribute('src', 'assets/images/image1_2.jpg');
 }
-
+// FONCTION LORSQU'ON SORT LA SOURIS DE L'IMAGE
 function mouseOut() {
   image.setAttribute('src', 'assets/images/image1.jpg');
 }
